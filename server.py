9a16abfaba5904a668dca86aee1ce1e407b1b23f
rkrkrediffmail <rkrk@rediffmail.com>
2024-04-08 09:42:43 +0530

    Update server.py

--- server.py
+++ server.py
@@ -5,15 +5,22 @@ import socketserver
 from http import HTTPStatus
 
 
-class Handler(http.server.SimpleHTTPRequestHandler):
+class CustomHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(HTTPStatus.OK)
-        self.end_headers()
-        msg = 'Hello! you requested %s' % (self.path)
-        self.wfile.write(msg.encode())
+        script_name = self.path.strip('/')
+        if script_name.endswith('.py'):
+            try:
+                module = __import__(script_name[:-3])
+                module.main(self)
+            except ImportError:
+                self.send_response(HTTPStatus.NOT_FOUND)
+                self.end_headers()
+                self.wfile.write(b'404 - Not Found')
+        else:
+            super().do_GET()
 
 
 port = int(os.getenv('PORT', 80))
 print('Listening on port %s' % (port))
-httpd = socketserver.TCPServer(('', port), Handler)
+httpd = socketserver.TCPServer(('', port), CustomHandler)
 httpd.serve_forever()
